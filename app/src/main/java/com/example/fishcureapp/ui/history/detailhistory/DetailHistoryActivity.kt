8d66d4f7bc1f28370453f00b875e77cc75c37416
safitrisa <saafiitrii09@gmail.com>
2024-06-02 14:10:09 +0700

    add ui features

---
+++ app/src/main/java/com/example/fishcureapp/ui/history/detailhistory/DetailHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fishcureapp.ui.history.detailhistory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fishcureapp.R
+
+class DetailHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_history)
+    }
+}
